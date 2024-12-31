cb0da097f40b64a84ba81625805685c45df282fe
loesen-maurovigliero <91024327+mauVig@users.noreply.github.com>
2024-12-30 21:50:08 -0300

    30/12

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,13 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors:{
+			AdBlue:'#0078ff',
+			AdDarkBlue:'#000755',
+			red:"#ff0000",
+			textGray:'#ededed',
+			textBlack:'#151515'
+		}
 	},
 	plugins: [],
 }
